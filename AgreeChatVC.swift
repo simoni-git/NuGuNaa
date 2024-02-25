0dfbb68d9b7e0c92bfbdfe5a6a17936c93b63e6f
시모니(Simoni) <145527459+simoni-git@users.noreply.github.com>
2024-02-26 01:00:10 +0900

    채팅뷰완성
    채팅방처럼 구현하기 완성 - 모든작업 끝인듯 -

--- AgreeChatVC.swift
+++ AgreeChatVC.swift
@@ -10,22 +10,19 @@ import Alamofire
 
 class AgreeChatVC: UIViewController {
     
-    var data: [String] = []
+    var data: [String] = ["곧 토론이 시작됩니다. 잠시만 기다려주세요"]
     var accessToken: String = ""
     var billNO: String = ""
     var position: Int = 0
     var billTitle: String = ""
-    
     
     var agreeChatVM: AgreeChatVM!
     var timer: Timer?
     var timer2: Timer?
     var timer3: Timer?
     
-    
     var remaining299Seconds = 119 // 타이머 초 초기값 설정 299로 바꿔야함
     var remaining119Seconds = 59 // 타이머 초 초기값 설정 119로 바꿔야함
-    
     
     @IBOutlet weak var contentsView: UIView!
     @IBOutlet weak var timerLabel: UILabel!
@@ -41,13 +38,6 @@ class AgreeChatVC: UIViewController {
         tableView.delegate = self
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100 // 테이블뷰셀의 대략적인 높이
-        
-       // getContentAPI() 타이머부분에 정각에 울리도록 해놨음.
-        
-        if self.data.isEmpty {
-            data.append("안녕하세요! 이번 토론 사회자를 맡게된 AI사회자 입니다. 이번 제목인 \(self.billTitle) 에 대해서 토론을 진행하도록 하겠습니다. 등록버튼은 한번 누르시면 수정되지 않으며 모든 채팅은 150자 이내로 작성해 주세요. 입론을 먼저 서술해 주세요!")
-            self.tableView.reloadData()
-         }
         timer?.fire()
         // 1초마다 checkTime 메서드를 호출하는 타이머 설정
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(checkTime), userInfo: nil, repeats: true)
@@ -82,7 +72,6 @@ class AgreeChatVC: UIViewController {
         DispatchQueue.main.async { [weak self] in
             self!.timerLabel.text = "제한시간: \(self!.remaining299Seconds)초"
         }
-        //timerLabel.text = "\(remaining299Seconds)초"
            
            // 0초일 때에는 타이머를 정지하고 초기화
            if remaining299Seconds == 0 {
@@ -114,7 +103,6 @@ class AgreeChatVC: UIViewController {
         DispatchQueue.main.async { [weak self] in
             self!.timerLabel.text = "대기하세요: \(self!.remaining119Seconds)초"
         }
-           //timerLabel.text = "\(remaining119Seconds)초"
            
            // 0초일 때에는 타이머를 정지하고 초기화
            if remaining119Seconds == 0 {
@@ -137,112 +125,110 @@ class AgreeChatVC: UIViewController {
         }
         
         // 15시 00분 00초 입장 get 호출 / 실험할때는 제거하고 뷰 입장하자마자 울리도록 구현해놓을것
-        if (hour == 00) && minute == 29 && second == 0 {
-           // getContentAPI() 동결
+        if (hour == 00) && minute == 07 && second == 0 {
+            getContentAPI()
             start299SecondTimer()
             registerBtn.isHidden = false
             
         }
         
         // 15시 06분 00초 post 호출
-        if (hour == 00) && minute == 31 && second == 0 {
-            //toGPTPost() 동결
+        if (hour == 00) && minute == 09 && second == 0 {
+            toGPTPost()
             start119SecondTimer()
         }
         
         // 15시 07분 00초 get 호출
-        if (hour == 00) && minute == 32 && second == 0 {
-            //serverGet() 동결
+        if (hour == 00) && minute == 10 && second == 0 {
+            serverGet()
             start299SecondTimer()
         }
         
         // 15시 13분 00초 post 호출
-        if (hour == 00) && minute == 34 && second == 0 {
-            //toGPTPost() 동결
+        if (hour == 00) && minute == 12 && second == 0 {
+            toGPTPost()
             start119SecondTimer()
         }
         
         // 15시 14분 00초 get 호출
-        if (hour == 00) && minute == 35 && second == 0 {
-           // serverGet() 동결
+        if (hour == 00) && minute == 13 && second == 0 {
+            serverGet()
             start299SecondTimer()
         }
         
         // 15시 20분 00초 post 호출
-        if (hour == 00) && minute == 37 && second == 0  {
-            // toGPTPost() 동결
+        if (hour == 00) && minute == 15 && second == 0  {
+             toGPTPost()
             start119SecondTimer()
         }
         
         // 15시 21분 00초 get 호출
-        if (hour == 00) && minute == 38 && second == 0  {
-            // serverGet() 동결
+        if (hour == 00) && minute == 16 && second == 0  {
+             serverGet()
             start299SecondTimer()
         }
         
         // 15시 27분 00초 post 호출
-        if (hour == 00) && minute == 40 && second == 0  {
-            // toGPTPost() 동결
+        if (hour == 00) && minute == 18 && second == 0  {
+             toGPTPost()
             start119SecondTimer()
         }
         
         // 15시 28분 00초 get 호출
-        if (hour == 00) && minute == 41 && second == 0  {
-            // serverGet() 동결
+        if (hour == 00) && minute == 19 && second == 0  {
+             serverGet()
             start299SecondTimer()
         }
         
         // 15시 34분 00초 post 호출
-        if (hour == 00) && minute == 43 && second == 0  {
-            // toGPTPost() 동결
+        if (hour == 00) && minute == 21 && second == 0  {
+             toGPTPost()
             start119SecondTimer()
         }
         
         // 15시 35분 00초 get 호출
-        if (hour == 00) && minute == 44 && second == 0  {
-           // serverGet() 동결
-            start299SecondTimer()
+        if (hour == 00) && minute == 22 && second == 0  {
+            serverGet()
+            registerBtn.isHidden = true
+            timerLabel.text = "토론종료"
+            textView.text = "토론이 종료되었습니다. 채팅방에서 나가주셔도 됩니다."
         }
         
     }
     
     func toGPTPost() {
-        print("toGPTPost 메서드 호출됨")
+        print("AgreeChatVC - toGPTPost() called 호출됨")
         postGPTAPI()
         
     }
     
     func serverGet() {
-        print("serverGet 메서드 호출됨")
+        print("AgreeChatVC - serverGet() called 호출됨")
         getContentAPI()
       }
-    
-    
-
       
     //MARK: - 등록하기 버튼 클릭
     @IBAction func tapRegisterBtn(_ sender: UIButton) {
         guard let text = textView.text else {return}
-       // myTextPostAPI()
+        myTextPostAPI()
+        self.data.append(text)
+        self.tableView.reloadData()
         self.textView.text = nil
         self.registerBtn.isHidden = true
     
-//        // 등록버튼 누르면 바로 최신데이터가 보일수있게 스크롤해줌
-//        let indexPath = IndexPath(row: self.data.count - 1, section: 0)
-//           self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-//        // 키보드 감추기
-//            self.view.endEditing(true)
-//            // 뷰 위치 원래대로 복구
-//            self.view.frame.origin.y = 0
+        // 등록버튼 누르면 바로 최신데이터가 보일수있게 스크롤해줌
+        let indexPath = IndexPath(row: self.data.count - 1, section: 0)
+           self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+        // 키보드 감추기
+            self.view.endEditing(true)
+            // 뷰 위치 원래대로 복구
+            self.view.frame.origin.y = 0
     }
     
    
     //MARK: - postGPT
     func postGPTAPI() {
-        print("찬성채팅자아앙~~~postGPTAPI calledddddddddd~~")
-        //타이머가 지나면서 포스트가울리던 여튼 포스트가 울릴때마다 qType 의 변수가 1씩 올라가도록 구현해야함.
-        
-        agreeChatVM.qType += 1
+        print("AgreeChatVC - postGPTAPI() called ")
         
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(self.accessToken)"
@@ -257,22 +243,15 @@ class AgreeChatVC: UIViewController {
                     // 요청 성공시, petitionResponse 변수에 파싱된 결과가 들어갑니다.
                     print("요청 성공: \(petitionResponse)")
                     
-                   
                 case .failure(let error):
                     print("요청 실패: \(error)")
                 }
             }
     }
     
-    
     //MARK: - postAPI
     func myTextPostAPI() {
-        
-        //타이머가 지나면서 포스트가울리던 여튼 포스트가 울릴때마다 qType 의 변수가 1씩 올라가도록 구현해야함.
-        //일단임시로 해보겠음
-        
-       print("myTextPostAPI 가 호출되었습니다. ")
-        
+       print("AgreeChatVC - myTextPostAPI() called  ")
         guard let text = self.textView.text else {return}
         
          let parameters: [String: Any] = [
@@ -304,6 +283,8 @@ class AgreeChatVC: UIViewController {
         func getContentAPI() {
             print("AgreeChatVC - getContentAPI() called")
             
+            agreeChatVM.qType += 1
+            
             let headers: HTTPHeaders = [
                 "Authorization": "Bearer \(self.accessToken)"
             ]
@@ -316,8 +297,16 @@ class AgreeChatVC: UIViewController {
                     case .success(let petitionResponse):
                         // 요청 성공시, petitionResponse 변수에 파싱된 결과가 들어갑니다.
                         print("요청 성공: \(petitionResponse)")
-                       
-                       
+                        self.data.removeAll()
+                        self.data.append("안녕하세요! 이번 토론 사회자를 맡게된 AI사회자 입니다. 이번 제목인 \(self.billTitle) 에 대해서 토론을 진행하도록 하겠습니다. 등록버튼은 한번 누르시면 수정되지 않으며 모든 채팅은 150자 이내로 작성해 주세요. 입론을 먼저 서술해 주세요!")
+                        let content = petitionResponse.map { $0.content }
+                        self.data.append(contentsOf: content)
+                        //self.data = petitionResponse.map { $0.content }
+                        self.tableView.reloadData()
+                        //등록버튼 누르면 바로 최신데이터가 보일수있게 스크롤해줌
+                       let indexPath = IndexPath(row: self.data.count - 1, section: 0)
+                          self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+                        
                     case .failure(let error):
                         print("요청 실패: \(error)")
                     }
@@ -364,42 +353,34 @@ class AgreeChatVC: UIViewController {
     }
     
     
-   
-    
 }
 
 
 extension AgreeChatVC: UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        data.count
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "AgreeChatCell") as? AgreeChatCell else {
             return UITableViewCell()
         }
-       
-        // 짝수 번째 셀인지 확인하여 레이아웃을 조정합니다.
+        
+        let data = data[indexPath.row]
+        cell.mentLabel.text = data
+        
         if indexPath.row % 2 == 0 {
-            cell.mentLabel.text = "AI사회자: \(data)"
-            // 짝수 번째 셀: 셀을 왼쪽에 붙이고 오른쪽 마진을 추가합니다.
-            cell.mentLabel.textAlignment = .left
-            cell.contentView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 8)
+            
+            cell.leadingConstant.constant = 10
+            cell.trailingConstant.constant = 70
         } else {
-            cell.mentLabel.text = "나: \(data)"
-            // 홀수 번째 셀: 셀을 오른쪽에 붙이고 왼쪽 마진을 추가합니다.
-            cell.mentLabel.textAlignment = .right
-            cell.contentView.layoutMargins = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 16)
-        }
+            
+            cell.leadingConstant.constant = 70
+            cell.trailingConstant.constant = 10
+            }
         
-        cell.layer.masksToBounds = false
-        cell.layer.shadowColor = UIColor.black.cgColor
-        cell.layer.shadowOpacity = 0.5
-        cell.layer.shadowOffset = CGSize(width: 0, height: 2)
-        cell.layer.shadowRadius = 10
-        cell.layer.cornerRadius = 10
-        
-        cell.contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0))
+        cell.subView.layer.cornerRadius = 10
+ 
         return cell
     }
     
@@ -416,12 +397,9 @@ extension AgreeChatVC: UITableViewDelegate , UITableViewDataSource {
 class AgreeChatCell: UITableViewCell {
     
     @IBOutlet weak var mentLabel: UILabel!
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0 , bottom: 5, right: 0))
-        
-    }
+    @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var trailingConstant: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstant: NSLayoutConstraint!
+ 
 }
 
--- ResultDetailVC.swift
+++ ResultDetailVC.swift
@@ -17,6 +17,7 @@ class ResultDetailVC: UIViewController {
     
     var accessToken: String = ""
     var billNO: String = ""
+    var billName: String = ""
     
     var resultDetailVM: ResultDetailVM!
     
@@ -27,13 +28,14 @@ class ResultDetailVC: UIViewController {
         tableView.delegate = self
         
         getGPTEndAPI()
+        titleLabel.text = billName
 
        
     }
     
     //MARK: - getGPT
     func getGPTEndAPI() {
-        print("ResultDetailVC - getGPTEndAPI() called 마지막겟 가즈아아아아아")
+        print("ResultDetailVC - getGPTEndAPI() called 마지막겟 가즈아아아아아 빌넘버는 \(self.billNO)")
         
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(self.accessToken)"
@@ -47,7 +49,9 @@ class ResultDetailVC: UIViewController {
                 case .success(let petitionResponse):
                     // 요청 성공시, petitionResponse 변수에 파싱된 결과가 들어갑니다.
                     print("요청 성공: \(petitionResponse)")
-                   
+                    self.data = petitionResponse.map { $0.content }
+                       // 테이블 뷰를 리로드하여 데이터를 표시
+                       self.tableView.reloadData()
                    
                 case .failure(let error):
                     print("요청 실패: \(error)")
@@ -68,16 +72,49 @@ extension ResultDetailVC: UITableViewDataSource , UITableViewDelegate {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell") as? ResultCell else {
             return UITableViewCell()
         }
+        let data = data[indexPath.row]
+        cell.resultLabel.text = data
         
+        if indexPath.row % 2 == 0 {
+            
+            cell.leadingConstant.constant = 10
+            cell.trailingConstant.constant = 70
+        } else {
+            
+            cell.leadingConstant.constant = 70
+            cell.trailingConstant.constant = 10
+            }
         
-        cell.layer.masksToBounds = false
-        cell.layer.shadowColor = UIColor.black.cgColor
-        cell.layer.shadowOpacity = 0.5
-        cell.layer.shadowOffset = CGSize(width: 0, height: 2)
-        cell.layer.shadowRadius = 10
-        cell.layer.cornerRadius = 10
+        cell.subView.layer.cornerRadius = 10
         
-        cell.contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0))
+//        // 데이터 설정
+//            let content = self.data[indexPath.row]
+//            cell.resultLabel.text = content
+//            
+//            // 짝수 인덱스는 왼쪽, 홀수 인덱스는 오른쪽으로 밀착
+//        
+//               if indexPath.row % 2 == 0 {
+//                   cell.resultLabel.text = "찬성측: \(content)"
+//                   // 짝수 번째 셀: 셀을 왼쪽에 붙이고 오른쪽 마진을 추가합니다.
+//                   cell.resultLabel.textAlignment = .left
+//                   cell.contentView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 8)
+//               } else {
+//                   cell.resultLabel.text = "반대측: \(content)"
+//                   // 홀수 번째 셀: 셀을 오른쪽에 붙이고 왼쪽 마진을 추가합니다.
+//                   cell.resultLabel.textAlignment = .right
+//                   cell.contentView.layoutMargins = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 16)
+//               }
+//          
+//        
+//        cell.layer.masksToBounds = false
+//        cell.layer.shadowColor = UIColor.black.cgColor
+//        cell.layer.shadowOpacity = 0.5
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 2)
+//        cell.layer.shadowRadius = 10
+//        cell.layer.cornerRadius = 10
+//        cell.contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0))
+//        
+        
         
         return cell
     }
@@ -96,14 +133,19 @@ extension ResultDetailVC: UITableViewDataSource , UITableViewDelegate {
 
 class ResultCell: UITableViewCell {
     
+    @IBOutlet weak var subView: UIView!
     @IBOutlet weak var resultLabel: UILabel!
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0 , bottom: 5, right: 0))
-        
-    }
+    @IBOutlet weak var trailingConstant: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstant: NSLayoutConstraint!
+    
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        
+//        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0 , bottom: 5, right: 0))
+//        
+//    }
     
     
 }
